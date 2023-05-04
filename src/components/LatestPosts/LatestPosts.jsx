6e64b7180b948a09d0db05700c68d43e6a000d36
Your Name <you@example.com>
2023-05-05 02:38:59 +0600

    fix: bug in LatestPosts

--- src/components/LatestPosts/LatestPosts.jsx
+++ src/components/LatestPosts/LatestPosts.jsx
@@ -2,17 +2,15 @@ import React from "react";
 import css from "./LatestPosts.module.css";
 import Card from "./Components/Card";
 import Slider from "react-slick";
-import styled from "styled-components"
-import SliderComp from './Components/SliderComp';
-
-
+import styled from "styled-components";
+import SliderComp from "./Components/SliderComp";
 
 const LatestPosts = () => {
 	return (
 		<section className={css.LatestPostsContainer}>
 			<h2 className={css.title}>Latest Posts</h2>
 			<Slide>
-        <SliderComp/>
+				<SliderComp />
 			</Slide>
 		</section>
 	);
@@ -20,65 +18,74 @@ const LatestPosts = () => {
 
 export default LatestPosts;
 
-
 const Slide = styled.div`
-  padding: 0 10%;
+	padding: 0 10%;
 
-  .slick-list,
-.slick-track {
-	padding: 5rem 0;
-}
-
-.center .CARD{
-	opacity: 0.7;
-	pointer-events: none;
-	width: 362px;
-  height: 482px;
-}
-.slick-dots .slick-active > ::before{
-	transform: scale(1.5);
-	color: var(--MainOrange) !important;
-}
-
-.center .slick-center .CARD{
-	transform: scale(1.1);
-	transition: all 400ms ease-in-out;
-  
-	opacity: 1;
-	position: relative;
-	pointer-events: all;
-}
-.center .bkg{
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  border-radius: 10px;
-  top:0;
-  left: 0;
-  right: 0;
-  z-index: -1;
-}
-.center .slick-center .CARD > .bkg{
-	background: linear-gradient(170deg,#000 20% 20%,hsla(0,0%,100%,0)) !important;
-}
-
-@media (max-width:1024px) {
-	.center .slick-center .CARD{
-		transform: scale(1);
-		z-index: inherit;
-		background-color: inherit;
-
+	.slick-list,
+	.slick-track {
+		padding: 5rem 0;
 	}
-	.center .CARD{
-		opacity: 1;
-		pointer-events: all;	
-	}	
-}
 
-  .slick-arrow::before{
-    display: none;
-  }
-  @media (max-width: 600){
-    padding: 0 5%;
-  }
-`
+	.center .CARD {
+		opacity: 0.7;
+		pointer-events: none;
+		width: 362px;
+		height: 482px;
+	}
+	.slick-dots .slick-active > ::before {
+		transform: scale(1.5);
+		color: var(--MainOrange) !important;
+	}
+
+	.center .slick-center .CARD {
+		transform: scale(1.1);
+		transition: all 400ms ease-in-out;
+		opacity: 1;
+		position: relative;
+		pointer-events: all;
+		color: #fff !important;
+	}
+	.center .slick-center .CARD > div h4 {
+		color: #fff !important;
+	}
+	.center .slick-center .CARD > div + div p {
+		color: #fff !important;
+	}
+
+	.center .bkg {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		border-radius: 10px;
+		top: 0;
+		left: 0;
+		right: 0;
+		z-index: -1;
+	}
+	.center .slick-center .CARD > .bkg {
+		background: linear-gradient(
+			170deg,
+			#000 20% 20%,
+			hsla(0, 0%, 100%, 0)
+		) !important;
+	}
+
+	@media (max-width: 1024px) {
+		.center .slick-center .CARD {
+			transform: scale(1);
+			z-index: inherit;
+			background-color: inherit;
+		}
+		.center .CARD {
+			opacity: 1;
+			pointer-events: all;
+		}
+	}
+
+	.slick-arrow::before {
+		display: none;
+	}
+	@media (max-width: 600) {
+		padding: 0 5%;
+	}
+`;
