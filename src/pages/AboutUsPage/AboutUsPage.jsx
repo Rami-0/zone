bc62f2236c22797fc2c811abb1e6d51fc6e6c4c0
Your Name <you@example.com>
2023-05-08 17:37:15 +0600

    add: Faq to About

--- src/pages/AboutUsPage/AboutUsPage.jsx
+++ src/pages/AboutUsPage/AboutUsPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import LatestPosts from "../../components/LatestPosts/LatestPosts";
+import FAQ from './../../components/FAQ/FAQ';
 
 const AboutUsPage = () => {
 	return (
 		<section>
 			<h5>AboutUsPage</h5>
       <LatestPosts/>
+			<FAQ/>
 		</section>
 	);
 };
