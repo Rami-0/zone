6d79ca9a45106b9d1b1aa2fd6be4f5b8978bf654
Rami-0 <Ramysh11@icloud.com>
2023-05-17 17:41:49 +0600

    fix

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
 import LatestPosts from './../../components/LatestPosts/LatestPosts';
 import FirstSection from './Components/firstSection/FirstSection';
 import SecondSection from './Components/secondSection/SecondSection';
@@ -7,8 +6,8 @@ import FAQ from './../../components/FAQ/FAQ';
 import OurServicesAndWorkFlow from './Components/OurServicesAndWorkFlow/OurServicesAndWorkFlow';
 import OurWork from './Components/OurWork/OurWork';
 
+
 const LandingPage = () => {
-	const navigator = useNavigate();
 	return (
 		<section>
 			<FirstSection/>
